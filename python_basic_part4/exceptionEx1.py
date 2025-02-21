15f8f42fb18354e6ab44bc0aed55a77cd922cec4
DESKTOP-MNQV98N\songhyun <songhyun0323@konkuk.ac.kr>
2025-02-21 16:08:34 +0900

    python_exceoption

---
+++ python_basic_part4/exceptionEx1.py
@@ -0,0 +1,12 @@
+try:
+    x = int(input('나눌 숫자를 입력하세요:'))
+    y = 10 / x
+    print(y)
+except ValueError as e:
+    print(e)
+except ZeroDivisionError as e:
+    print(e)
+except Exception as e:
+    print(e)
+
+
